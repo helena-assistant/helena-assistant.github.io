ffa1c28a48ab3678eab71c8b32d471407e9261e5
montsouz <guilhermesouzamont@gmail.com>
2021-07-20 22:49:07 -0300

    feat: ratings feedback

--- js/rating.js
+++ js/rating.js
@@ -5,6 +5,8 @@ const rate2 = document.getElementById("rate-2");
 const rate3 = document.getElementById("rate-3");
 const rate4 = document.getElementById("rate-4");
 const rate5 = document.getElementById("rate-5");
+const rateForm = document.getElementById("rate-form");
+const submitSuccess = document.getElementById("submit-success");
 
 const rates = [rate1, rate2, rate3, rate4, rate5];
 
@@ -24,7 +26,8 @@ const sendRating = (event) => {
   };
 
   http.send(params);
+  rateForm.style.display = "none";
+  submitSuccess.style.display = "block";
 };
 
-const rateForm = document.getElementById("rate-form");
 rateForm.addEventListener("submit", sendRating);
